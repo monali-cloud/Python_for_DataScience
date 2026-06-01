b1f7ba8c35bdf3ee31b22e070c029023bd054b7b
Monali Nagardhankar <nmonali12@gmail.com>
2026-06-01 13:01:51 +0530

    Add files via upload

---
+++ printnumbers.py
@@ -0,0 +1,22 @@
+class mynumbers:
+    def __iter__(self):
+        self.a =1
+        return self
+    
+    def __next__(self):
+        x=self.a
+        self.a += 1
+        return x
+    
+myclass = mynumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+    
+    
+ 
