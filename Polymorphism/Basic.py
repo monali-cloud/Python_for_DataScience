814af96e194fcc3ed436f9f3f64d965110779f5f
Monali Nagardhankar <nmonali12@gmail.com>
2026-06-11 13:01:13 +0530

    Add files via upload

---
+++ Polymorphism/Basic.py
@@ -0,0 +1,20 @@
+##Create two classes:
+
+##Dog with method sound() that prints "Woof!"
+##Cat with method sound() that prints "Meow!"
+
+
+class Dog():
+    def sound(self):
+        print("Woof!")
+        
+class Cat():
+    def sound(self):
+        print("Meow")
+        
+d1=Dog()
+c1=Cat()
+          
+d1.sound()
+c1.sound()                          
+        
