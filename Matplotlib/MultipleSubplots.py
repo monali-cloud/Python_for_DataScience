2b627d334443ddeed6341debae4b2eb432faa367
Monali Nagardhankar <nmonali12@gmail.com>
2026-07-27 18:37:30 +0530

    Add files via upload

---
+++ Matplotlib/MultipleSubplots.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+plt.subplot(1,2,1)
+plt.plot([1,2,3],[3,2,1])
+
+plt.subplot(1,2,2)
+plt.plot([1,2,3],[1,2,3])
+
+plt.subplot(2,1,1)
+plt.plot([1,2,3],[1,2,4])
+
+plt.show()
